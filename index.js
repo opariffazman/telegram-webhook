ce1ee50135e0cee4c4b2a2c47c6d5b8496d2f106
opariffazman <opariffazman@gmail.com>
2022-10-03 17:20:20 +0800

    remove telegraf and used axios

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { Telegraf } = require('telegraf')
 const express = require('express')
+const axios = require('axios')
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const token = process.env.TOKEN
-const bot = new Telegraf(token)
 
 app.post(`/webhook/${token}`, async (req, res) => {
   console.log(req.body)
@@ -14,7 +14,10 @@ app.post(`/webhook/${token}`, async (req, res) => {
   const chatId = req.body.message.chat.id
   const text = req.body.message.text
 
-  bot.telegram.sendMessage(chatId, 'Welcome', {})
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: chatId,
+    text: text
+  })
 
   console.log(`text: ${text} chatId: ${chatId} `)
   return res.send()
