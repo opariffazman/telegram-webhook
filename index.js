89deb3e26d6cac5d675795751627f18897225200
opariffazman <opariffazman@gmail.com>
2022-10-03 13:58:57 +0800

    fix id

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ app.use(express.urlencoded({ extended: true }))
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 app.post('/', (req, res) => {
-  const chat = req.body
-  const id = chat.message.id
-  const text = chat.message.text
+  const json = req.body
+  const id = json.message.chat.id
+  const text = json.message.text
+
+  if (text == 'hello') {
+    bot.telegram.sendMessage(id, 'hello', {})
+
+  }
+
+  if (text == 'salam') {
+    bot.telegram.sendMessage(id, 'wasalam', {})
+  }
 
   bot.telegram.sendMessage(id, 'yo', {})
 
-  console.log(`text: ${text} id: ${id}`)
+  console.log(`text: ${text} id: ${id} `)
   res.status(200).json('webhook received')
 })
 
